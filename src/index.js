e88d82a709ed07bde68dcef9f5a323d292b2125d
Prince Lidhoriya <157975792+prince07lid@users.noreply.github.com>
2025-07-25 17:34:43 +0530

    commented out once more

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //updated index.js file
+//updated once more
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
